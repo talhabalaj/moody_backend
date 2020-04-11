407d607840e8ef3bc6e29b2f3ceb91e56a19ee39
Talha Balaj <talhabalaj@hotmail.com>
2020-04-11 12:10:25 +0500

    added helmet, cors

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,18 @@
 import Express from 'express';
 import bodyParser from 'body-parser';
+import cors from 'cors';
+const app = Express();
 
 import { mainRouter } from './routes/v1';
 import { port } from './config';
 import './lib/db';
 import { escapeBody } from './middleware/escapeBody';
+import helmet from 'helmet';
 
-const app = Express();
 
 // Middleware
+app.use(helmet());
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(escapeBody);
