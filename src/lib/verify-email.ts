31208c91c54c4f439f0658de1ad9b105286fc4a8
Talha Balaj <talhabalaj@hotmail.com>
2020-05-12 21:39:44 +0500

    ...

--- src/lib/verify-email.ts
+++ src/lib/verify-email.ts
@@ -6,13 +6,11 @@ export const verifyEmail = async (email: string) => {
     const request = await axios.get(
       `http://apilayer.net/api/check?access_key=${mailboxlayerApi}&email=${email}&smtp=1&format=1`
     );
-    const parsedData = JSON.parse(request.data);
-    console.log(parsedData);
     if (request.status == 200) {
-      return parsedData["smtp_check"];
+      return request.data["smtp_check"];
     }
   } catch (e) {
-    console.error("Error:", e);
+    return true;
   }
 
   return false;
