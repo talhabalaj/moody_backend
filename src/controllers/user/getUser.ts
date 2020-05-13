fafb43446e2e2c0f6fe1925bc19eb231232783fd
Talha Balaj <talhabalaj@hotmail.com>
2020-05-13 15:54:05 +0500

    fix!

--- src/controllers/user/getUser.ts
+++ src/controllers/user/getUser.ts
@@ -29,6 +29,6 @@ export const getUser = async (req: ExpressRequest, res: ExpressResponse) => {
   } else
     return createError(res, {
       code: 404,
-      args: [`User with username "${userName}" is not found`],
+      args: [`User is not found`],
     });
 };
