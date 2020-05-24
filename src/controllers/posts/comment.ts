7344bff78ecd0e467a813bc78c428bde099fdee1
Talha Balaj <talhabalaj@hotmail.com>
2020-05-24 21:55:40 +0500

    delete comment controller

--- src/controllers/posts/comment.ts
+++ src/controllers/posts/comment.ts
@@ -46,3 +46,52 @@ export const createComment = async (
     args: ["The post you are looking for doesnt exist"],
   });
 };
+
+export const deleteComments = async (
+  req: ExpressRequest,
+  res: ExpressResponse
+) => {
+  assert(req.user, "[deleteComment] should be called with req.user");
+  const { commentId } = req.params;
+  const { user } = req;
+  const comment = await Comment.findById(commentId);
+
+  if (comment) {
+    const post = await Post.findById(comment.post);
+
+    if (post) {
+      if (
+        String(post.user) === String(user?._id) ||
+        String(comment.user) === String(user?.id)
+      ) {
+        // current user owns the post
+        // or the coment was made by the current user
+
+        // if comment has replies delete them
+        if (comment.replies && comment.replies.length > 0) {
+          await Comment.deleteMany({ _id: { $in: comment.replies } });
+        }
+        // remove comment reference in post
+        post.comments.pull(comment._id);
+        await post.save();
+
+        // finally delete the comment
+        await comment.remove();
+
+        return createResponse(res, {
+          status: 200,
+          message: "Successfull",
+        });
+      }
+    }
+    return createError(res, {
+      code: 403,
+      args: ["Comment cannot be deleted."],
+    });
+  } else {
+    return createError(res, {
+      code: 404,
+      args: ["The comment you want to delete doesn't exist"],
+    });
+  }
+};
