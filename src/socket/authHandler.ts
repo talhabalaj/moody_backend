de0e5c0f5cd0af4f8eb08d9cc26b3d3ad02e801b
Talha Balaj <talhabalaj@hotmail.com>
2020-05-25 15:38:33 +0500

    creating rooms based on convestion id add people to rooms

--- src/socket/authHandler.ts
+++ src/socket/authHandler.ts
@@ -3,10 +3,16 @@ import jwt, { JsonWebTokenError } from "jsonwebtoken";
 import { AuthToken } from "../models/AuthToken";
 import { secret } from "../config";
 import { User } from "../models/User";
+import { Conversation } from "../models/Conversation";
 
 export const socketAuthHandler = async (socket: Socket, next: Function) => {
-  if (socket.handshake.query && socket.handshake.query.token) {
+  if (
+    socket.handshake.query &&
+    socket.handshake.query.token &&
+    socket.handshake.query.conversation
+  ) {
     const token = socket.handshake.query.token;
+    const conversationId = socket.handshake.query.conversation;
     const tokenInfo = await AuthToken.findOne({ token });
     if (tokenInfo?.isValid) {
       try {
@@ -22,7 +28,17 @@ export const socketAuthHandler = async (socket: Socket, next: Function) => {
         if (user) {
           socket.request.user = user;
           socket.request.tokenInfo = tokenInfo;
-          return next();
+
+          const conversationObj = await Conversation.findById(conversationId);
+          if (
+            conversationObj &&
+            conversationObj.members.includes(String(user._id))
+          ) {
+            socket.request.conversation = conversationObj;
+            socket.join(conversationId);
+            return next();
+          }
+          return next(Error("Invalid conversation Id."));
         }
       } catch (e) {
         return next(e);
--- src/socket/index.ts
+++ src/socket/index.ts
@@ -1,12 +1,16 @@
 import { Socket } from "socket.io";
+import { IConversation } from "../models/Conversation";
+import { IUser } from "../models/User";
 
 export function socketListener(socket: Socket) {
-  socket.on("message", (msg) => {
-    console.log("message: " + msg);
-    socket.broadcast.emit("message", msg);
-  });
+  const conversation: IConversation = socket.request.conversation;
+  const user: IUser = socket.request.user;
 
-  socket.on("disconnect", () => {
-    console.log("DISCONNECTED.");
+  const currentRoom = socket.nsp.to(conversation._id);
+
+  socket.on("message", async (msg) => {
+    console.log(`message from ${socket.request.user.userName}:` + msg);
+    const message = await conversation.sendMessage(msg, user._id);
+    currentRoom.emit("message", message);
   });
 }
