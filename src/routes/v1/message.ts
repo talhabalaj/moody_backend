d300261c288b8d7721bca8b1f83ac96cfe489284
Talha Balaj <talhabalaj@hotmail.com>
2020-05-25 17:34:45 +0500

    fixed typo

--- src/routes/v1/message.ts
+++ src/routes/v1/message.ts
@@ -1,16 +1,20 @@
 import { Router } from "express";
 import { authProvider, protectedRoute } from "../../middleware/auth";
-import { createConvo, getConversations } from "../../controllers/message";
+import {
+  createConvo,
+  getConversations,
+  getMessages,
+} from "../../controllers/message";
 
 const messageRouter = Router();
 
 messageRouter.get("/init/:to", authProvider, protectedRoute, createConvo);
 messageRouter.get("/all", authProvider, protectedRoute, getConversations);
 messageRouter.get(
-  "/:conversationId/messages",
+  "/:conversationId",
   authProvider,
   protectedRoute,
-  getConversations
+  getMessages
 );
 
 export { messageRouter };
