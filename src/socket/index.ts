f3e3b5ff095925cd49f89815076352993f8b31f3
Talha Balaj <talhabalaj@hotmail.com>
2020-05-26 10:50:11 +0500

    added unique id to each socket event

--- src/socket/index.ts
+++ src/socket/index.ts
@@ -2,14 +2,22 @@ import { Socket } from "socket.io";
 import { IConversation } from "../models/Conversation";
 import { IUser } from "../models/User";
 
+interface SendMessage {
+  message: string;
+  uniqueId: string;
+}
+
 export function socketListener(socket: Socket) {
   const conversation: IConversation = socket.request.conversation;
   const user: IUser = socket.request.user;
 
   const currentRoom = socket.nsp.to(conversation._id);
 
-  socket.on("message", async (msg) => {
-    const message = await conversation.sendMessage(msg, user._id);
-    currentRoom.emit("message", JSON.stringify(message));
+  socket.on("message", async (msg: SendMessage) => {
+    const message = await conversation.sendMessage(msg.message, user._id);
+    currentRoom.emit(
+      "message",
+      JSON.stringify({ uniqueId: msg.uniqueId, message })
+    );
   });
 }
